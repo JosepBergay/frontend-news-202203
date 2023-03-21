e403559ba83656b27fa8e50067fd098b132438cc
jberg <josep.bergay@gmail.com>
2023-03-21 16:58:29 +0100

    feat: add relative date component

---
+++ components/RelativeDate.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+
+dayjs.extend(relativeTime)
+
+defineProps<{ date: string | number | Date }>()
+
+</script>
+
+<template>
+  <span>{{ dayjs().to(dayjs(date)) }}</span>
+</template>
