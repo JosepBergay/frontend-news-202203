c5a834128f1f0624e6e8642e03143a7e0688ee7f
jberg <josep.bergay@gmail.com>
2023-03-21 16:57:46 +0100

    feat: add animated number component

---
+++ components/AnimatedNumber.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref, reactive, watch, watchEffect } from 'vue'
+import gsap from 'gsap'
+
+const props = defineProps<{ n: number }>()
+// const number = ref(0)
+const tweened = reactive({
+  number: 0,
+})
+
+watchEffect(() => {
+  gsap.to(tweened, { duration: 5, number: Number(props.n) || 0 })
+})
+</script>
+
+<template>
+  <span>{{ tweened.number.toFixed(0) }}</span>
+</template>
